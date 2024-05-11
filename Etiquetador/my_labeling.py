1ca5c751df0e5897c273fd8b1386c04bb286eeac
Paul-DanielPop <paul-daniel02@outlook.com>
2024-05-11 20:33:58 +0200

    get_shape_accuracy terminado

--- Etiquetador/my_labeling.py
+++ Etiquetador/my_labeling.py
@@ -4,7 +4,7 @@ __group__ = 'TO_BE_FILLED'
 import numpy as np
 
 from utils_data import read_dataset, read_extended_dataset, crop_images, visualize_retrieval
-import KNN as knn
+from KNN import __authors__, __group__, KNN
 from Kmeans import __authors__, __group__, KMeans, distance, get_colors
 
 
@@ -39,6 +39,16 @@ def retrieval_combined(images, shape_tags, color_tags, shape, color):
         visualize_retrieval(matching_images, 10)
 
 
+def get_shape_accuracy(tags, ground_truth):
+    if len(tags) != len(ground_truth):
+        raise ValueError("Length of 'tags' and 'ground_truth' must be the same.")
+
+    total = len(tags)
+    correct = sum(1 for prediction, truth in zip(tags, ground_truth) if prediction == truth)
+
+    return (correct / total) * 100 if total > 0 else 0
+
+
 if __name__ == '__main__':
     # Load all the images and GT
     train_imgs, train_class_labels, train_color_labels, test_imgs, test_class_labels, \
@@ -53,11 +63,22 @@ if __name__ == '__main__':
 
     # You can start coding your functions here
 
+    """
+    Tests qualitativos
+    """
     #retrieval_by_color(test_imgs, test_color_labels, ['Black', 'Blue'])
     #retrieval_by_shape(test_imgs, test_class_labels, 'Jeans')
     #retrieval_by_shape(test_imgs, test_class_labels, 'Shorts')
     #retrieval_by_shape(test_imgs, test_class_labels, 'Heels')
-    retrieval_combined(test_imgs, test_class_labels, test_color_labels, 'Flip Flops', ['Blue'])
-    a = knn.KNN(train_imgs, train_class_labels)
+    #retrieval_combined(test_imgs, test_class_labels, test_color_labels, 'Flip Flops', ['Blue'])
+
+    """
+        Tests quantitativos
+    """
+    kn = KNN(train_imgs, train_class_labels)
+    kn.predict(test_imgs, 60)
+
+    shape_percent = get_shape_accuracy(kn.get_class(), test_class_labels)
+    print("Percentatge: ", round(shape_percent, 2), "%")
 
     # prueba retrieval_by_color
