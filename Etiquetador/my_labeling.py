1ec8eabb334e77465554950a23fbe9576438afa1
Paul-DanielPop <paul-daniel02@outlook.com>
2024-05-25 17:54:12 +0200

    commit de tests

--- Etiquetador/my_labeling.py
+++ Etiquetador/my_labeling.py
@@ -7,7 +7,7 @@ import numpy as np
 import utils
 import matplotlib.pyplot as plt
 
-#from utils_data import read_dataset, read_extended_dataset, crop_images, visualize_retrieval
+# from utils_data import read_dataset, read_extended_dataset, crop_images, visualize_retrieval
 from utils_data import *
 from KNN import __authors__, __group__, KNN
 from Kmeans import __authors__, __group__, KMeans, distance, get_colors
@@ -75,7 +75,7 @@ def kmeans_knn_statistics(knn_train_images, knn_train_gt, test_images, num_image
                           kmeans_options=None, show_graph=False, show_image=False, view_statistics=False):
     global_statistics = []
     knn = KNN(knn_train_images, knn_train_gt)
-    result_shape_labels = knn.predict(imgs, 5)
+    result_shape_labels = knn.predict(test_images, 5)
     random_indices = random.sample(range(0, len(test_images) - 1), num_images)
     for i in random_indices:
         statistics = []
@@ -123,11 +123,12 @@ def kmeans_knn_statistics(knn_train_images, knn_train_gt, test_images, num_image
         visualize_statistics(global_statistics)
 
 
-def kmeans_statistics_nonRandom(train_images, train_class_gt, images_to_classify, color_gt, class_gt, kmax, show_graph=False,
-                      show_image=False, view_statistics=False, options=None):
+def kmeans_statistics_nonRandom(train_images, train_class_gt, images_to_classify, color_gt, class_gt, kmax,
+                                show_graph=False,
+                                show_image=False, view_statistics=False, options=None):
     global_statistics = []
     knn = KNN(train_images, train_class_gt)
-    result_shape_labels = knn.predict(imgs, 5)
+    result_shape_labels = knn.predict(images_to_classify, 5)
 
     for i, image in enumerate(images_to_classify):
         image = images_to_classify[i]
@@ -171,11 +172,13 @@ def kmeans_statistics_nonRandom(train_images, train_class_gt, images_to_classify
     if view_statistics:
         visualize_statistics(global_statistics)
 
-def kmeans_statistics_nonRandom_plusF(train_images, train_class_gt, images_to_classify, color_gt, class_gt, kmax, show_graph=False,
-                      show_image=False, view_statistics=False, options=None):
+
+def kmeans_statistics_nonRandom_plusF(train_images, train_class_gt, images_to_classify, color_gt, class_gt, kmax,
+                                      show_graph=False,
+                                      show_image=False, view_statistics=False, options=None):
     global_statistics = []
     knn = KNN(train_images, train_class_gt)
-    result_shape_labels = knn.predict(imgs, 5)
+    result_shape_labels = knn.predict(images_to_classify, 5)
 
     for i, image in enumerate(images_to_classify):
         image = images_to_classify[i]
@@ -296,31 +299,33 @@ def test_retrieval_combined(images, color_gt, shape_gt):
 
 
 def test_kmeans_statistics():
-    #images_to_classify = cropped_images[:10]
+    number_of_images_to_classify = 250
+
+    """
     opt = {
         'km_init': 'first'
     }
-    number_of_images_to_classify = 2
-    kmeans_knn_statistics(train_imgs, train_class_labels, cropped_images, number_of_images_to_classify,
-                      color_labels, class_labels, 5, opt, False, False, True)
+
+    kmeans_knn_statistics(train_imgs, train_class_labels, test_imgs, number_of_images_to_classify,
+                          test_color_labels, test_class_labels, 5, opt, False, False, True)
+
+    """
+    """
+    opt = {
+        'km_init': 'random'
+    }
+
+    kmeans_knn_statistics(train_imgs, train_class_labels, test_imgs, number_of_images_to_classify,
+                          test_color_labels, test_class_labels, 5, opt, False, False, True)
+
 
     """
     opt = {
         'km_init': 'kmeans++'
     }
 
-    images_to_classify = cropped_images[:2]
-    kmeans_statistics(train_imgs, train_class_labels, images_to_classify,
-                      color_labels, class_labels, 5, True, True, True, options=opt)
-
-    opt = {
-        'km_init': 'random'
-    }
-
-    images_to_classify = cropped_images[:2]
-    kmeans_statistics(train_imgs, train_class_labels, images_to_classify,
-                      color_labels, class_labels, 5, True, True, True, options=opt)
-    """
+    kmeans_knn_statistics(train_imgs, train_class_labels, test_imgs, number_of_images_to_classify,
+                          test_color_labels, test_class_labels, 5, opt, False, False, True)
 
 
 def visualize_statistics_fisher(statistics):
@@ -331,11 +336,10 @@ def visualize_statistics_fisher(statistics):
 
     # Inicializar listas para guardar las medias
     Ks = [stat['K'] for stat in statistics[0]]
-    fisher_avg = []
+    color_accuracy_avg = []
 
-    # Calcular la media para cada K
     for i in range(len(Ks)):
-        fisher_avg.append(np.mean([image_stats[i]['FISHER'] for image_stats in statistics]))
+        color_accuracy_avg.append(np.mean([image_stats[i]['Color_accuracy'] for image_stats in statistics]))
 
     for idx, image_stats in enumerate(statistics):
         # Extraer los valores de las estadísticas para cada K
@@ -349,17 +353,12 @@ def visualize_statistics_fisher(statistics):
         axs[0].tick_params(axis='both', which='major', labelsize=8)
         axs[0].grid(True)
 
-    # Gráficos promedio
-    axs[1].plot(Ks, fisher_avg, marker='o', label='Average', color='black')
-    axs[1].set_title('Fisher Coeficient vs K (Average)', fontsize=10)
-    axs[1].set_xlabel('Number of Clusters (K)', fontsize=8)
-    axs[1].set_ylabel('Fisher Coeficient', fontsize=8)
-    axs[1].tick_params(axis='both', which='major', labelsize=8)
-    axs[1].grid(True)
-
-    # Añadir leyenda a cada gráfico
-    for ax in axs:
-        ax.legend()
+        axs[1].plot(Ks, color_accuracy_avg, marker='o', label='Average', color='black')
+        axs[1].set_title('Color Accuracy vs K (Average)', fontsize=10)
+        axs[1].set_xlabel('Number of Clusters (K)', fontsize=10)
+        axs[1].set_ylabel('Color Accuracy (%)', fontsize=10)
+        axs[1].tick_params(axis='both', which='major', labelsize=8)
+        axs[1].grid(True)
 
     # Ajustar espacio entre gráficos
     plt.tight_layout(pad=4.0)
@@ -369,7 +368,8 @@ def visualize_statistics_fisher(statistics):
 
 
 def visualize_statistics(statistics):
-    fig, axs = plt.subplots(2, 3, figsize=(14, 7))
+    fig, axs = plt.subplots(2, 2, figsize=(14, 7))
+    fig2, axs2 = plt.subplots(2, 2, figsize=(14, 7))
 
     num_images = len(statistics)
     colors = plt.cm.get_cmap('tab10', num_images)
@@ -379,18 +379,21 @@ def visualize_statistics(statistics):
     WCDs_avg = []
     convergence_times_avg = []
     color_accuracy_avg = []
+    num_iter_avg = []
 
     # Calcular la media para cada K
     for i in range(len(Ks)):
         WCDs_avg.append(np.mean([image_stats[i]['WCD'] for image_stats in statistics]))
         convergence_times_avg.append(np.mean([image_stats[i]['Convergence_time'] for image_stats in statistics]))
         color_accuracy_avg.append(np.mean([image_stats[i]['Color_accuracy'] for image_stats in statistics]))
+        num_iter_avg.append(np.mean([image_stats[i]['Num_iterations'] for image_stats in statistics]))
 
     for idx, image_stats in enumerate(statistics):
         # Extraer los valores de las estadísticas para cada K
         WCDs = [stat['WCD'] for stat in image_stats]
         convergence_times = [stat['Convergence_time'] for stat in image_stats]
         color_accuracy = [stat['Color_accuracy'] for stat in image_stats]
+        num_iter = [stat['Num_iterations'] for stat in image_stats]
 
         # Graficar WCD vs K
         axs[0, 0].plot(Ks, WCDs, marker='o', label=f'Image {idx + 1}', color=colors(idx))
@@ -407,41 +410,55 @@ def visualize_statistics(statistics):
         axs[0, 1].tick_params(axis='both', which='major', labelsize=8)
         axs[0, 1].grid(True)
         # Graficar precisión vs K
-        axs[0, 2].plot(Ks, color_accuracy, marker='o', label=f'Image {idx + 1}', color=colors(idx))
-        axs[0, 2].set_title('Accuracy vs K', fontsize=10)
-        axs[0, 2].set_xlabel('Number of Clusters (K)', fontsize=10)
-        axs[0, 2].set_ylabel('Accuracy (%)', fontsize=10)
-        axs[0, 2].tick_params(axis='both', which='major', labelsize=8)
-        axs[0, 2].grid(True)
+        axs[1, 0].plot(Ks, color_accuracy, marker='o', label=f'Image {idx + 1}', color=colors(idx))
+        axs[1, 0].set_title('Color Accuracy vs K', fontsize=10)
+        axs[1, 0].set_xlabel('Number of Clusters (K)', fontsize=10)
+        axs[1, 0].set_ylabel('Color Accuracy (%)', fontsize=10)
+        axs[1, 0].tick_params(axis='both', which='major', labelsize=8)
+        axs[1, 0].grid(True)
+
+        axs[1, 1].plot(Ks, num_iter, marker='o', label=f'Image {idx + 1}', color=colors(idx))
+        axs[1, 1].set_title('Number of Iterations vs K', fontsize=10)
+        axs[1, 1].set_xlabel('Number of Clusters (K)', fontsize=10)
+        axs[1, 1].set_ylabel('Number of Iterations', fontsize=10)
+        axs[1, 1].tick_params(axis='both', which='major', labelsize=8)
+        axs[1, 1].grid(True)
 
     # Gráficos promedio
-    axs[1, 0].plot(Ks, WCDs_avg, marker='o', label='Average', color='black')
-    axs[1, 0].set_title('Within-Class-Distance (WCD) vs K (Average)', fontsize=10)
-    axs[1, 0].set_xlabel('Number of Clusters (K)', fontsize=8)
-    axs[1, 0].set_ylabel('WCD', fontsize=8)
-    axs[1, 0].tick_params(axis='both', which='major', labelsize=8)
-    axs[1, 0].grid(True)
+    axs2[0, 0].plot(Ks, WCDs_avg, marker='o', label='Average', color='black')
+    axs2[0, 0].set_title('Within-Class-Distance (WCD) vs K (Average)', fontsize=10)
+    axs2[0, 0].set_xlabel('Number of Clusters (K)', fontsize=8)
+    axs2[0, 0].set_ylabel('WCD', fontsize=8)
+    axs2[0, 0].tick_params(axis='both', which='major', labelsize=8)
+    axs2[0, 0].grid(True)
 
-    axs[1, 1].plot(Ks, convergence_times_avg, marker='o', label='Average', color='black')
-    axs[1, 1].set_title('Convergence Time vs K (Average)', fontsize=10)
-    axs[1, 1].set_xlabel('Number of Clusters (K)', fontsize=8)
-    axs[1, 1].set_ylabel('Convergence Time (seconds)', fontsize=8)
-    axs[1, 1].tick_params(axis='both', which='major', labelsize=8)
-    axs[1, 1].grid(True)
+    axs2[0, 1].plot(Ks, convergence_times_avg, marker='o', label='Average', color='black')
+    axs2[0, 1].set_title('Convergence Time vs K (Average)', fontsize=10)
+    axs2[0, 1].set_xlabel('Number of Clusters (K)', fontsize=8)
+    axs2[0, 1].set_ylabel('Convergence Time (seconds)', fontsize=8)
+    axs2[0, 1].tick_params(axis='both', which='major', labelsize=8)
+    axs2[0, 1].grid(True)
 
-    axs[1, 2].plot(Ks, color_accuracy_avg, marker='o', label='Average', color='black')
-    axs[1, 2].set_title('Accuracy vs K (Average)', fontsize=10)
-    axs[1, 2].set_xlabel('Number of Clusters (K)', fontsize=10)
-    axs[1, 2].set_ylabel('Accuracy (%)', fontsize=10)
-    axs[1, 2].tick_params(axis='both', which='major', labelsize=8)
-    axs[1, 2].grid(True)
+    axs2[1, 0].plot(Ks, color_accuracy_avg, marker='o', label='Average', color='black')
+    axs2[1, 0].set_title('Color Accuracy vs K (Average)', fontsize=10)
+    axs2[1, 0].set_xlabel('Number of Clusters (K)', fontsize=10)
+    axs2[1, 0].set_ylabel('Color Accuracy (%)', fontsize=10)
+    axs2[1, 0].tick_params(axis='both', which='major', labelsize=8)
+    axs2[1, 0].grid(True)
 
-    # Añadir leyenda a cada gráfico
-    for ax in axs.flat:
-        ax.legend()
+    axs2[1, 1].plot(Ks, num_iter_avg, marker='o', label='Average', color='black')
+    axs2[1, 1].set_title('Number of Iterations vs K (Average)', fontsize=10)
+    axs2[1, 1].set_xlabel('Number of Clusters (K)', fontsize=10)
+    axs2[1, 1].set_ylabel('Number of Iterations', fontsize=10)
+    axs2[1, 1].tick_params(axis='both', which='major', labelsize=8)
+    axs2[1, 1].grid(True)
+
+    for ax in axs2.flat:
+        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize='small')
 
     # Ajustar espacio entre gráficos
-    plt.tight_layout(pad=4.0)
+    fig.tight_layout(pad=3.0, w_pad=3.0, h_pad=3.0)
+    fig2.tight_layout(pad=3.0, w_pad=3.0, h_pad=3.0)
 
     # Mostrar los gráficos
     plt.show()
@@ -468,6 +485,7 @@ def MyPlot3DCloud(km, rows=1, cols=1, spl_id=1, title=''):
     ax.set_title(title)
     return ax
 
+
 if __name__ == '__main__':
     # Load all the images and GT
     train_imgs, train_class_labels, train_color_labels, test_imgs, test_class_labels, \
@@ -492,9 +510,11 @@ if __name__ == '__main__':
     # test_retrieval_combined(train_imgs[:300], train_color_labels[:300], train_class_labels[:300])
 
     """Tests kmeans_statistics"""
-    #test_kmeans_statistics()
-    
+    test_kmeans_statistics()
+
+
     """Test Best_K"""
+    """
     print("_________________________Best_K___________________________")
     opt1 = {
         'km_init': 'kmeans++'
@@ -502,6 +522,7 @@ if __name__ == '__main__':
     opt2 = {
         'km_init': 'random'
     }
+    """
     """
     # Base img_01 -->
     print("____________________IMAGE 01_____________________")
@@ -575,8 +596,10 @@ if __name__ == '__main__':
                       color_labels, class_labels, 10, True, True, True, options=opt2)
     
     """
+    """
     print("____________________IMAGE 01_____________________")
     print("Base:")
-    images_to_classify = test_imgs[:10]
+    images_to_classify = cropped_images[:100]
     kmeans_statistics_nonRandom_plusF(train_imgs, train_class_labels, images_to_classify,
-                       color_labels, class_labels, 10, False, False, True, options=None)
+                                      color_labels, class_labels, 5, False, False, True, options=None)
+    """
